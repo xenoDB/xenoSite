e033cf3bb39eb85b88234a44e0685e850c22b831
painfueg0 <kystax69@gmail.com>
2025-07-20 11:27:20 +0530

    upd footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,32 +1,41 @@
 import { SiNpm } from "react-icons/si";
 import { Link } from "react-router-dom";
+import { BiCopyright } from "react-icons/bi";
 import { FaGithubSquare } from "react-icons/fa";
 import { IoLogoDiscord } from "react-icons/io5";
 
+const logoSize = "text-2xl";
+const animation = "hover:scale-125 transition-transform duration-150";
+
+const leftElement = (
+  <div className={`flex items-center gap-1 text-[#333333EE]`}>
+    <BiCopyright className={logoSize} />
+    <b>1sT-Services ({new Date().getFullYear()})</b>
+  </div>
+);
+
+const rightElement = (
+  <div className="flex gap-3">
+    <Link to="https://npmjs.com/org/xenodb" target="_blank">
+      <SiNpm className={`text-[#CB0000EE] ${logoSize} ${animation}`} />
+    </Link>
+
+    <Link to="https://github.com/xenodb" target="_blank">
+      <FaGithubSquare className={`text-[#333333EE] ${logoSize} ${animation}`} />
+    </Link>
+
+    <Link to="https://discord.gg/1st-952570101784281139" target="_blank">
+      <IoLogoDiscord className={`text-[#5663F2EE] ${logoSize} ${animation}`} />
+    </Link>
+  </div>
+);
+
 export default function Footer() {
   return (
-    <footer className="bg-gradient-to-b from-[#FFFFFF] to-[#DBEAFECC] text-white py-2">
+    <footer className="text-[#444444] py-2">
       <div className="flex justify-between items-center container mx-auto px-4">
-        <div className="flex flex-row gap-4">
-          <p className="text-center text-[#444444]">
-            <strong className="flex items-center gap-2">
-              <span className="text-[20px]">&copy;</span> 1sT-Services (
-              {new Date().getFullYear()})
-            </strong>
-          </p>
-        </div>
-
-        <div className="flex flex-row gap-4">
-          <Link to="https://www.npmjs.com/org/xenodb" target="_blank">
-            <SiNpm className="text-[#CB0000EE] text-2xl" />
-          </Link>
-          <Link to="https://www.github.com/xenodb" target="_blank">
-            <FaGithubSquare className="text-[#333333EE] text-2xl" />
-          </Link>
-          <Link to="https://discord.gg/1st-952570101784281139" target="_blank">
-            <IoLogoDiscord className="text-[#5663F2EE] text-2xl" />
-          </Link>
-        </div>
+        {leftElement}
+        {rightElement}
       </div>
     </footer>
   );
