069c1c706639e5b42c06943d030ed5c06abc870d
painfueg0 <kystax69@gmail.com>
2025-07-20 11:38:34 +0530

    follow up

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,12 +4,13 @@ import { BiCopyright } from "react-icons/bi";
 import { FaGithubSquare } from "react-icons/fa";
 import { IoLogoDiscord } from "react-icons/io5";
 
-const logoSize = "text-2xl";
-const animation = "hover:scale-125 transition-transform duration-150";
+const iconSize = "text-2xl";
+const iconAnimation = "hover:scale-125 transition-transform duration-150";
+const iconStyles = `${iconSize} ${iconAnimation}`;
 
 const leftElement = (
-  <div className={`flex items-center gap-1 text-[#333333EE]`}>
-    <BiCopyright className={logoSize} />
+  <div className="flex gap-1 text-[#333333EE]">
+    <BiCopyright className={iconSize} />
     <b>1sT-Services ({new Date().getFullYear()})</b>
   </div>
 );
@@ -17,22 +18,22 @@ const leftElement = (
 const rightElement = (
   <div className="flex gap-3">
     <Link to="https://npmjs.com/org/xenodb" target="_blank">
-      <SiNpm className={`text-[#CB0000EE] ${logoSize} ${animation}`} />
+      <SiNpm className={`text-[#CB0000EE] ${iconStyles}`} />
     </Link>
 
     <Link to="https://github.com/xenodb" target="_blank">
-      <FaGithubSquare className={`text-[#333333EE] ${logoSize} ${animation}`} />
+      <FaGithubSquare className={`text-[#333333EE] ${iconStyles}`} />
     </Link>
 
     <Link to="https://discord.gg/1st-952570101784281139" target="_blank">
-      <IoLogoDiscord className={`text-[#5663F2EE] ${logoSize} ${animation}`} />
+      <IoLogoDiscord className={`text-[#5663F2EE] ${iconStyles}`} />
     </Link>
   </div>
 );
 
 export default function Footer() {
   return (
-    <footer className="text-[#444444] py-2">
+    <footer className="py-2">
       <div className="flex justify-between items-center container mx-auto px-4">
         {leftElement}
         {rightElement}
